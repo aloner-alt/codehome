32ffa3ebe3785df826336f0773a135ee44386565
aloner-alt <makcgiper93@gmail.com>
2026-03-07 19:01:11 +0300

    first commit1

--- homework1.ts
+++ homework1.ts
@@ -190,55 +190,101 @@
 
 
 
-function sum(a:number, b:number): number {
-  return a + b;
-}
+// function sum(a:number, b:number): number {
+//   return a + b;
+// }
 
-function isPositive(num:number): boolean {
-  return num > 0;
-}
+// function isPositive(num:number): boolean {
+//   return num > 0;
+// }
 
-function getLength(str:string): number {
-  return str.length;
-}
-
-
-function max(a:number, b:number): number {
-  return a > b ? a : b;
-}
-
-function canVote(age:number): boolean {
-  return age >= 18;
-}
+// function getLength(str:string): number {
+//   return str.length;
+// }
 
 
-function sumArray(arr:number[]):number {
-  return arr.reduce((acc, num) => acc + num, 0);
-}
+// function max(a:number, b:number): number {
+//   return a > b ? a : b;
+// }
 
-function getLongWords(words:string[]):string[] {
-  return words.filter(word => word.length > 5);
-}
-
-function doubleNumbers(arr:number[]):number[] {
-  return arr.map(num => num * 2);
-}
-
-function greet(name:string, title?:string): string {
-  return title 
-    ? `Привет, ${title} ${name}!`
-    : `Привет, ${name}!`;
-}
-
-function calculatePrice(price:number, discount:number = 0): number {
-  return price - price * (discount / 100);
-}
+// function canVote(age:number): boolean {
+//   return age >= 18;
+// }
 
 
-const numbers: number[] = [1, 2, 3, 4, 5];
+// function sumArray(arr:number[]):number {
+//   return arr.reduce((acc, num) => acc + num, 0);
+// }
 
-numbers.forEach(function(num:number): void {
-  console.log(num);
-});
+// function getLongWords(words:string[]):string[] {
+//   return words.filter(word => word.length > 5);
+// }
 
-const multiplyByThree = (num:number): number => num * 3;
+// function doubleNumbers(arr:number[]):number[] {
+//   return arr.map(num => num * 2);
+// }
+
+// function greet(name:string, title?:string): string {
+//   return title 
+//     ? `Привет, ${title} ${name}!`
+//     : `Привет, ${name}!`;
+// }
+
+// function calculatePrice(price:number, discount:number = 0): number {
+//   return price - price * (discount / 100);
+// }
+
+
+// const numbers: number[] = [1, 2, 3, 4, 5];
+
+// numbers.forEach(function(num:number): void {
+//   console.log(num);
+// });
+
+// const multiplyByThree = (num:number): number => num * 3;
+
+
+
+
+//1.1
+
+// let df:string = "лелуш"
+// console.log (df.length)
+// console.log (df.toUpperCase())
+// console.log (df.toLowerCase())
+
+
+// let text:string = "JavaScript — это круто! Я учу JavaScript."
+// console.log(text.includes("JavaScript"))
+// console.log(text.indexOf("JavaScript"))
+// console.log(text.lastIndexOf("JavaScript"))
+
+
+// let text:string = "photo.jpg"
+// console.log(text.startsWith("photo"))
+// console.log(text.endsWith(".jpg"))
+// console.log(text.endsWith(".png"))
+
+
+
+
+
+
+// let text:string = "TypeScript"
+// console.log(text.slice(4,-1))
+// let data:string = "2024-03-15"
+// console.log(data.split('-'))
+
+
+
+// let love:string = "я люблю яблоки, яблоки вкусные"
+// console.log(love.replaceAll("яблоки", "бананы"))
+
+
+// let admin:string = " admin " 
+// console.log(admin.trim())
+
+// console.log("*".repeat(10))
+
+
+// console.log("typescript".toUpperCase())
